a319721600ce6c7028b3683ede289a9d0f1fd1fe
reyy1231 <rayneralvi@gmail.com>
2020-03-25 18:02:22 +0700

    week7

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Hero } from '../hero';
+import { DataService } from '../data.service';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
+})
+export class MainComponent implements OnInit {
+  heroes: Hero[];
+  error:boolean;
+  displayedColumns: string[] = ['heroName', 'realName', 'skill', 'universe'];
+
+  constructor(
+    private ds: DataService,
+  ) {}
+
+  ngOnInit(): void {
+    this.ds.getHeroes().subscribe(
+      response => {
+        this.heroes = response as Hero[];
+      },
+      err => {
+        console.log(err);
+        this.error = true;
+      }
+    );
+  }
+
+}
